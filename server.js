0d063668a1b0653fb93a0e2d01439d7273c93383
Chandler Chilvers <cwchilvers@gmail.com>
2024-05-02 17:15:48 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8000,
+    mediaroot: './media',
+    allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: './ffmpeg/ffmpeg.exe',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  }
+};
+
+const nms = new NodeMediaServer(config);
+nms.run();
